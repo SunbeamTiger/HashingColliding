6dfa5549f689eb40875b0d66a5b6dafdc6f00d51
danhagen <danhagen@outlook.com>
2024-06-04 12:24:41 -0700

    Add project files.

---
+++ HashingColliding/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace HashingColliding
+{
+    internal class Program
+    {
+        static string[] storage = new string[1000];
+        static void Main(string[] args)
+        {
+            string name = "Dan";
+            int result = getHashKey(name);
+            storage[result] = name;
+            Console.WriteLine($"{name} is stored at {result}");
+
+            name = "Dna";
+            result = getHashKey(name);
+            storage[result] = name;
+            Console.WriteLine($"{name} is stored at {result}");
+
+            name = "Trevor";
+            result = getHashKey(name);
+            storage[result] = name;
+            Console.WriteLine($"{name} is stored at {result}");
+
+            name = "Bob";
+            result = getHashKey(name);
+            storage[result] = name;
+            Console.WriteLine($"{name} is stored at {result}");
+        }
+        static int getHashKey(string name)
+        {
+            char c;
+            int ASCIITotal = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                c = (char)name[i];
+                ASCIITotal += (int)c;
+            }
+            return ASCIITotal % storage.Length;
+        }
+    }
+}
